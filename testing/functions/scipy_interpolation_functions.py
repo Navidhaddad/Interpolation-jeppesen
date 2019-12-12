ed2ad46d74a58451553c3f92ea8dd2d509dfd674
JoRo987 <58259891+JoRo987@users.noreply.github.com>
2019-12-12 12:01:58 +0100

    Update scipy_interpolation_functions.py

--- testing/functions/scipy_interpolation_functions.py
+++ testing/functions/scipy_interpolation_functions.py
@@ -4,13 +4,20 @@ import numpy as np
 import json 
 import scipy
 
-def scipy_interpolation_linear (data, interpolation_point):
+def scipy_interpolation_linear (data, interpolation_point, parameter):
     
-    y = data.FUELFLOW
-    X = data.drop(['FUELFLOW'], axis=1)
+    if (parameter == 'FUELFLOW' ) :
+        
+        y = data.FUELFLOW
+        X = data.drop(['FUELFLOW'], axis=1)
+        
+    elif (parameter == 'DRAG'):
+        
+        y = data.DRAG
+        X = data.drop(['DRAG'], axis=1)
    
     
-    points , values = closest_points(X,y,interpolation_point,2500)  
+    points , values = closest_points(X,y,interpolation_point,2500,parameter)  
                                                                
     
     interpolated_val = scipy.interpolate.griddata(points, values, interpolation_point, method='linear', rescale='TRUE')
@@ -19,13 +26,20 @@ def scipy_interpolation_linear (data, interpolation_point):
     
     return interpolated_val
 
-def scipy_interpolation_nearest (data, interpolation_point):
+def scipy_interpolation_nearest (data, interpolation_point,parameter):
     
-    y = data.FUELFLOW
-    X = data.drop(['FUELFLOW'], axis=1)
+    if (parameter == 'FUELFLOW'):
+    
+        y = data.FUELFLOW
+        X = data.drop(['FUELFLOW'], axis=1)
+        
+    elif (parameter == 'DRAG'):
+        
+        y = data.DRAG
+        X = data.drop(['DRAG'], axis=1)
    
     
-    points , values = closest_points(X,y,interpolation_point,2500)  
+    points , values = closest_points(X,y,interpolation_point,2500,parameter)  
                                                                
     
     interpolated_val = scipy.interpolate.griddata(points, values, interpolation_point, method='nearest', rescale='TRUE')
@@ -35,17 +49,29 @@ def scipy_interpolation_nearest (data, interpolation_point):
     return interpolated_val
     
 
-def closest_points(X,y,interpolation_point, n):
+def closest_points(X,y,interpolation_point, n, parameter):
     X['distance'] = X.sub(interpolation_point).pow(2).sum(1).pow(0.5) #calculate euclidean distance
     X = X.sort_values(['distance']).iloc[0:n] #sort
     X = X.drop(['distance'],axis=1)
-    X = X.reset_index() 
+    X = X.reset_index()
+    
     X.columns = ['index_', 'DISA', 'ALTITUDE', 'MASS','MACH'] #rename columns
     
-    y.columns = ['index_', 'FUELFLOW']
-    X = X.join(y, on = 'index_', how='left',) #join 20 nearest point with 
-                                                                #corresonding fuel flow
-    val = X['FUELFLOW']
-    poi = X.drop(['index_','FUELFLOW'], axis=1)
+    if (parameter == 'FUELFLOW'):
+        y.columns = ['index_', 'FUELFLOW']
+        X = X.join(y, on = 'index_', how='left',) #join 20 nearest point with 
+                                                                    #corresonding fuel flow
+        val = X['FUELFLOW']
+        poi = X.drop(['index_','FUELFLOW'], axis=1)
+     
+    
+    elif (parameter == 'DRAG'):
+        
+        y.columns = ['index_', 'DRAG']
+        X = X.join(y, on = 'index_', how='left',) #join 20 nearest point with 
+                                                                    #corresonding fuel flow
+        val = X['DRAG']
+        poi = X.drop(['index_','DRAG'], axis=1)
+     
     
     return poi , val
